aa689a8af4676749ab26f70301ce6f8a770e8b18
Tobias Michael Fischbach <fischbach.tobias.michael.1995@gmail.com>
2019-11-10 00:07:31 +0100

    20191110 saving files does work

--- app/src/main/java/com/example/verzettelt/editor.java
+++ app/src/main/java/com/example/verzettelt/editor.java
@@ -6,6 +6,8 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.yydcdut.markdown.MarkdownConfiguration;
 import com.yydcdut.markdown.MarkdownEditText;
@@ -33,18 +35,52 @@ import rx.schedulers.Schedulers;
 
 public class editor extends AppCompatActivity {
 
-    private RxMDEditText mRxMDEditText;
-    private RxMDTextView mRxMDTextView;
-    private RxMDConfiguration mRxMDConfiguration;
+    //private RxMDEditText mRxMDEditText;
+    //private RxMDTextView mRxMDTextView;
+    //private RxMDConfiguration mRxMDConfiguration;
 
-    private MarkdownTextView mMarkdownTextView;
-    private MarkdownEditText mMarkdownEditText;
-    private MarkdownProcessor mMarkdownProcessor;
+    //private MarkdownTextView mMarkdownTextView;
+    //private MarkdownEditText mMarkdownEditText;
+    //private MarkdownProcessor mMarkdownProcessor;
+
+    EditText myZettel;
+    identifier currentId = new identifier();
+    String filename = currentId.create() + ".md";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+        myZettel = findViewById(R.id.zettelContextEditText);
+    }
+
+    // Saving Zettel to internal Storage
+    public void saveZettel(View view)
+    {
+        //Get
+        String input = myZettel.getText().toString();
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput(filename, MODE_PRIVATE);
+            fos.write(input.getBytes());
+
+            myZettel.getText().clear();
+            Toast.makeText(this, "Zettel is " + filename +" is save!",
+                    Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 
     public void onClickClose(View view) {
--- app/src/main/java/com/example/verzettelt/fileManagement.java
+++ app/src/main/java/com/example/verzettelt/fileManagement.java
@@ -6,6 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -15,15 +21,24 @@ import java.util.Date;
 public class fileManagement extends AppCompatActivity {
     identifier currentId = new identifier();
     String id = currentId.create();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_management);
-        TextView myAwesomeTextView = (TextView)findViewById(R.id.idTextView);
+        TextView myAwesomeTextView = (TextView) findViewById(R.id.idTextView);
         myAwesomeTextView.setText(id);
+
+        //Using API Call in order to get a list of all files
+        File file = getFilesDir();
+        File f[] = file.listFiles();
+        
     }
+
     public void onClickClose(View view) {
         finish();
     }
+
+
 
 }
